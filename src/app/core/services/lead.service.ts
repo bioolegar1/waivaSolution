8baea4961aec096353731638928f89e17cb46e94
bioolegar1 <bioolegari@gmail.com>
2026-02-24 19:28:37 -0300

    finalizaçao do site

--- src/app/core/services/lead.service.ts
+++ src/app/core/services/lead.service.ts
@@ -6,7 +6,7 @@ export class LeadService {
     private http = inject(HttpClient);
 
     // Certifique-se de que esta URL é a da ÚLTIMA implementação (Versão mais recente)
-    private readonly GOOGLE_URL = 'https://script.google.com/macros/s/AKfycbzVx7J8xtY1PstPI0Sv_ZqIz4LjJw_Blnc3Zu-clsf8Exid97R_uC4RVXfGNoGpqfeR/exec';
+    private readonly GOOGLE_URL = 'https://script.google.com/macros/s/AKfycbz_M3wHZPKJ8FMjOiQetXI5XTUptqDOChOp64QSutRK1T0RnDIov9ir13d8Q1Jt_XLF8Q/exec';
 
     sendLead(data: any) {
         // O Content-Type 'text/plain' é essencial para evitar bloqueios de CORS no Apps Script
